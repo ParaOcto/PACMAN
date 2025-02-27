0a519341226eb9e9f3a052890ac2ce37ef0086c9
phon <ndphon23@clc.fitus.edu.vn>
2025-02-27 20:51:41 +0700

    27-02

--- map.py
+++ map.py
@@ -1,5 +1,6 @@
 import pygame
 from board import boards
+import math
 
 pygame.init()
 
@@ -9,19 +10,27 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 pygame.display.set_caption("Pacman")
 fps = 60
+color = 'blue'
+PI = math.pi
 font = pygame.font.Font('freesansbold.ttf', 20)
 level = boards
+counter = 0
+flicker = False
+player_x = 450
+player_y = 663
+player_images = []
+for i in range(1, 5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'player_images/{i}.png'), (45, 45)))
 
 def draw_board():
-    color = 'blue'
     num1 = ((HEIGHT - 50) // 32)
-    num2 = (WIDTH // 32)
+    num2 = (WIDTH // 30)
     for i in range(len(level)):
         for j in range(len(level[i])):
             if level[i][j] == 1:
-                pygame.draw.circle(screen, 'white', (j * num2  + (0.5 * num2), i * num1 + 0.5 * num1), 4)
-            if level[i][j] == 2:
-                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + 0.5 * num1), 10)
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
+            if level[i][j] == 2 and not flicker:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
             if level[i][j] == 3:
                 pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
                                  (j * num2 + (0.5 * num2), i * num1 + num1), 3)
@@ -30,26 +39,37 @@ def draw_board():
                                  (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
             if level[i][j] == 5:
                 pygame.draw.arc(screen, color, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1],
-                                0, 3.14 / 2, 3)
+                                0, PI / 2, 3)
             if level[i][j] == 6:
                 pygame.draw.arc(screen, color,
-                                [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], 3.14 / 2, 3.14, 3)
+                                [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
             if level[i][j] == 7:
-                pygame.draw.arc(screen, color, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], 3.14,
-                                3 * 3.14 / 2, 3)
+                pygame.draw.arc(screen, color, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI,
+                                3 * PI / 2, 3)
             if level[i][j] == 8:
                 pygame.draw.arc(screen, color,
-                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * 3.14 / 2,
-                                2 * 3.14, 3)
+                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2,
+                                2 * PI, 3)
             if level[i][j] == 9:
                 pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
                                  (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
 
+def draw_player():
+    screen.blit(player_images[counter // 5], (player_x, player_y))
+
 run = True
 while run:
     timer.tick(fps)
+    if counter < 19:
+        counter += 1
+        if counter > 3:
+            flicker = False
+    else:
+        counter = 0
+        flicker = True
     screen.fill('black')    
     draw_board()
+    draw_player()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
